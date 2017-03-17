440e6a0302960978b33976776a490e6805a302b7
Miguel de Andrés Herrero <migueldah91@gmail.com>
2017-03-17 17:20:53 +0100

    Proyecto

---
+++ ProyectosIG-VS13/Hola/escena.cpp
@@ -0,0 +1,136 @@
+#include "escena.h"
+#include <GL/freeglut.h>
+
+//-------------------------------------------------------------------------
+
+void Escena::init(){
+  // texturas
+  // luces
+}
+
+//-------------------------------------------------------------------------
+
+Escena::~Escena(){
+   // liberar memoria y recursos 
+}
+
+//-------------------------------------------------------------------------
+
+void Escena::draw(){
+  ejes.draw();
+  piramidetri.drawDiabolo();
+}
+
+//-------------------------------------------------------------------------
+
+Ejes::Ejes(GLdouble l) {
+  vertices[0].set(0, 0, 0);
+  vertices[1].set(l, 0, 0);
+  vertices[2].set(0, 0, 0);
+  vertices[3].set(0, l, 0);
+  vertices[4].set(0, 0, 0);
+  vertices[5].set(0, 0, l);
+
+  colores[0].set(1, 0, 0);
+  colores[1].set(1, 0, 0);
+  colores[2].set(0, 1, 0);
+  colores[3].set(0, 1, 0);
+  colores[4].set(0, 0, 1);
+  colores[5].set(0, 0, 1);
+}
+
+//-------------------------------------------------------------------------
+
+void Ejes::draw(){
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glVertexPointer(3, GL_DOUBLE, 0, &vertices[0]);
+  glEnableClientState(GL_COLOR_ARRAY);
+  glColorPointer(3, GL_DOUBLE, 0, colores);
+
+  glLineWidth(2);
+  glDrawArrays(GL_LINES, 0, 6); 
+  glLineWidth(1);
+
+  glDisableClientState(GL_COLOR_ARRAY);
+  glDisableClientState(GL_VERTEX_ARRAY);
+
+ /* 
+  glLineWidth(2);
+  glBegin(GL_LINES);
+     glColor3d(1.0, 0.0, 0.0); // red
+     glVertex3d(vertices[0].x, vertices[0].y, vertices[0].z);   // origin
+     glVertex3d(vertices[1].x, vertices[1].y, vertices[1].z);   // x   
+
+     glColor3d(0.0, 1.0, 0.0); // green
+     glVertex3d(vertices[2].x, vertices[2].y, vertices[2].z);   // origin
+     glVertex3d(vertices[3].x, vertices[3].y, vertices[3].z);	  // y
+
+     glColor3d(0.0, 0.0, 1.0); // blue
+     glVertex3d(vertices[4].x, vertices[4].y, vertices[4].z);   // origin
+     glVertex3d(vertices[5].x, vertices[5].y, vertices[5].z);	  // z   
+  glEnd();
+  glLineWidth(1);
+  */
+}
+
+//-------------------------------------------------------------------------
+void Triangulo::draw()
+{
+	activar();
+	glColor4d(0, 0, 0, 1);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawArrays(GL_TRIANGLES, 0, numDat);
+	desactivar();
+}
+
+void Triangulo::activar() {
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glNormalPointer(GL_DOUBLE, 0, normales.data());
+	glVertexPointer(3, GL_DOUBLE, 0, &vertices[0]);
+}
+
+void Triangulo::desactivar() {
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+}
+
+
+void PiramideTri::draw()
+{
+	//glTranslated(0, 25, 0);
+	//glRotated(-30, 0, 0, 1);
+	PVec3 *aux = t.vertices.data();
+	hl = 25;
+	aux[0].z = 2*hl;
+	aux[0].y = 0;
+	aux[0].x = 0;
+	t.vertices[0] = *aux;
+	t.draw();
+	aux = t3.vertices.data();
+	t2.draw();
+	//aux[2].y = hl;
+	aux[1].x = 0;
+	aux[1].y = 0;
+	aux[1].z = 2*hl;
+	t3.draw();
+
+	//t.draw();
+	
+}
+
+void PiramideTri::drawDiabolo()
+{
+	glTranslated(0, 2*hl, 0);
+	glRotated(90, 1, 0, 0);
+	draw();
+	glTranslated(0, 0, 4 * hl);
+	glRotated(180,1, 0, 0);
+	draw();
+	glRotated(60, 0, 0, 1);
+	draw();
+	glTranslated(0, 0, 4 * hl);
+	glRotated(180, 1, 0, 0);
+	draw();
+}
---
+++ ProyectosIG-VS13/Hola/escena.h
@@ -0,0 +1,128 @@
+//#pragma once
+#ifndef _H_escena_H_
+#define _H_escena_H_
+#include "tipos.h"
+#include <vector>
+#include "Math.h"
+#define _USE_MATH_DEFINES
+# define PI           3.14159265358979323846  /* pi */
+//-------------------------------------------------------------------------
+class Ejes {
+public:
+  Ejes(GLdouble l);
+  ~Ejes(){};
+  void draw();
+public:
+  PVec3 vertices[6];
+  PVec3 colores[6];
+};
+
+//-------------------------------------------------------------------------
+
+class Triangulo {
+public:
+	GLuint numDat;
+	std::vector<PVec3> vertices, normales;
+
+	Triangulo(int radio) {
+
+		numDat = 3;
+		PVec3 v0(radio, 0.0, 0.0);
+		PVec3 v1(radio * cos(2 * PI / 3), radio * sin(2 * PI / 3), 0.0);
+		PVec3 v2(radio * cos(2 * PI / 3), -radio * sin(2 * PI / 3), 0.0);
+		vertices = { v0, v1, v2 };
+
+
+		/////Normales
+		GLfloat Qx, Qy, Qz, Px, Py, Pz;
+
+		Px = v1.x - v0.x;
+		Py = v1.y - v0.y;
+		Pz = v1.z - v0.z;
+		Qx = v2.x - v0.x;
+		Qy = v2.y - v0.y;
+		Qz = v2.z - v0.z;
+		PVec3 NormalX = Py*Qz - Pz*Qy;
+		PVec3 NormalY = Pz*Qx - Px*Qz;
+		PVec3 NormalZ = Px*Qy - Py*Qx;
+
+		normales = { NormalX, NormalY, NormalZ };
+	}
+
+	~Triangulo() { numDat = 0; }
+
+	//bool load(char arch[]); //genera los datos
+	void draw();
+
+protected:
+	void activar();
+	void desactivar();
+};
+
+//------------------------Piramide----------------------------------------------
+
+class PiramideTri{
+	
+public:
+	Triangulo t,t2,t3;
+	int rad;
+	double hl;
+	PiramideTri(int radBase, double h) : t(radBase),t2(radBase),t3(radBase),rad(radBase), hl(h) {};
+
+	~PiramideTri() { }
+
+	//bool load(char arch[]); //genera los datos
+	void draw();
+	void drawDiabolo();
+	void rota();
+	
+protected:
+	void activar();
+	void desactivar();
+};
+
+
+class Rectangulo
+{
+	
+public:
+
+	CTex2 textura;
+	Color4 color;
+	std::vector<PVec3> vertices, normal;
+	Rectangulo(double ancho, double alto)
+	{
+		PVec3 v0(ancho/2, alto/2, 0.0);
+		PVec3 v1(ancho / 2, -alto / 2, 0.0);
+		PVec3 v2(-ancho / 2, alto / 2, 0.0);
+		PVec3 v3(-ancho / 2, -alto / 2, 0.0);
+		vertices = { v0, v1, v2,v3 };
+
+	}
+};
+
+//----------------------------TriAnimado----------------------------------
+
+class TriAnimado : Triangulo
+{
+	
+};
+
+
+//-------------------------------------------------------------------------
+
+class Escena {
+public:
+	Escena() : ejes(200), piramidetri(50,10) {};
+  ~Escena();
+  void init();
+  void draw();
+public:
+  Ejes ejes;
+  PiramideTri piramidetri;
+};
+
+
+
+
+#endif  // _H_escena_H_
